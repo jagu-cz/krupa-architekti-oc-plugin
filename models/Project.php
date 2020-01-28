1b77c1d7666f91bf20eee99d0a936d309dc69d5a
Marek Erben <erben.marek@gmail.com>
2020-01-28 15:26:17 +0100

    [#14084] gallery (wip)

--- models/Project.php
+++ models/Project.php
@@ -13,6 +13,8 @@ class Project extends Model
 
     protected $slugs = ['slug' => 'title'];
 
+    protected $jsonable = ['data'];
+
     public $attachOne = [
         'cover_photo' => \System\Models\File::class
     ];
@@ -25,7 +27,32 @@ class Project extends Model
 
     public function getPhotosCountAttribute()
     {
-        return 123; // FIXME
+        $count = 0;
+
+        foreach ($this->data as $row) {
+            foreach ($row['columns'] as $column) {
+                if ($column['content_type'] === 'image') {
+                    $count += 1;
+                }
+            }
+        }
+
+        return $count;
+    }
+
+    public function getTextCountAttribute()
+    {
+        $count = 0;
+
+        foreach ($this->data as $row) {
+            foreach ($row['columns'] as $column) {
+                if ($column['content_type'] === 'text') {
+                    $count += 1;
+                }
+            }
+        }
+
+        return $count;
     }
 
     public function beforeSave()
