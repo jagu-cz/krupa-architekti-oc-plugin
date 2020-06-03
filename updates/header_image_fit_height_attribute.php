d66d36468122da7e4665a5e2bad0783a16eff8c8
Marek Erben <erben.marek@gmail.com>
2020-06-03 15:52:26 +0200

    Allow header image to fit width/height

--- updates/header_image_fit_height_attribute.php
+++ updates/header_image_fit_height_attribute.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
 
-class FullWidthAttribute extends Migration
+class HeaderImageFitWidthAttribute extends Migration
 {
     private const PROJECTS_TABLE = 'jagu_krupaarchitekti_projects';
 
@@ -18,7 +18,7 @@ class FullWidthAttribute extends Migration
     public function up()
     {
         Schema::table(self::PROJECTS_TABLE, function (Blueprint $table) {
-            $table->boolean('full_width')->default('t');
+            $table->boolean('header_image_fit_height')->default('t');
         });
     }
 
@@ -30,7 +30,7 @@ class FullWidthAttribute extends Migration
     public function down()
     {
         Schema::table(self::PROJECTS_TABLE, function (Blueprint $table) {
-            $table->dropColumn('full_width');
+            $table->dropColumn('header_image_fit_height');
         });
     }
 }
