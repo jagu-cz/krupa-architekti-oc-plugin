8497094d9acb428a26ac84a732e8ec1e15933142
Marek Erben <erben.marek@gmail.com>
2020-09-08 18:17:47 +0200

    Add project caption attribute

--- models/Project.php
+++ models/Project.php
@@ -20,6 +20,7 @@ class Project extends Model
 
     public $rules = [
         'title' => 'required',
+        'caption' => '',
         'description' => 'required',
         'cover_image' => 'required',
     ];
